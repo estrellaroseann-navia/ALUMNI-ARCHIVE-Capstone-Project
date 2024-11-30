5b836df710360171a05447da767794a58dfbf4a5
ankara963 <128681738+ankara963@users.noreply.github.com>
2024-11-30 15:16:39 +0800

    updates

--- alumni-archive-laravel/app/Filament/Resources/CollegesResource.php
+++ alumni-archive-laravel/app/Filament/Resources/CollegesResource.php
@@ -8,12 +8,17 @@ use App\Models\Colleges;
 use Filament\Forms\Form;
 use Filament\Tables\Table;
 use Filament\Resources\Resource;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\Repeater;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
 use Illuminate\Database\Eloquent\Builder;
 use App\Filament\Resources\CollegesResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\CollegesResource\RelationManagers;
+use Filament\Forms\Components\Textarea;
 
 class CollegesResource extends Resource
 {
@@ -21,12 +26,38 @@ class CollegesResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+
+    public static function getNavigationGroup(): ?string
+    {
+        return 'Academic Management'; // Replace with your desired group name
+    }
+
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
                 TextInput::make('name')->label('College Name')->required(),
                 TextInput::make('description'),
+                Section::make('Programs')
+                    ->schema([
+                        Repeater::make('programs')
+                            ->relationship('programs')  // Assumes a relationship exists
+                            ->schema([
+                                TextInput::make('name')  // Program name
+                                    ->label('Program Name')
+                                    ->required(),
+
+                                Textarea::make('description')  // Program description
+                                    ->label('Program Description')
+                                    ->required(),
+
+                            ])
+                            ->columns(1)
+                            ->collapsible()
+                            ->nullable()  // Allow the repeater to be left empty
+                            ->default([])  // Allow the repeater to be left empty
+                    ]),
+
             ]);
     }
 
@@ -56,6 +87,11 @@ class CollegesResource extends Resource
         return [
             //
         ];
+    }
+
+    protected function afterCreate(): void
+    {
+        $this->redirect(CollegesResource::getUrl('index'));
     }
 
     public static function getPages(): array
